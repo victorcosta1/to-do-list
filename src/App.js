503c3143213dbae8f520bd90f9f9ca28187ea2d8
Victor costa <costavictor771@gmail.com>
2023-02-23 09:23:09 -0300

    To-do-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [task, setTask] = useState([])
+  const [input, setInput] =  useState()
+
+  function handleAdd(){
+    setTask([...task, input])
+  }
+
+  function handleDel(){
+    setTask([]).remove()
+  }
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Tarefas</h1>
+
+      <div>
+        <input type='text' value={input} placeholder='Digite algo aqui...' onChange={e => setInput(e.target.value)}/>
+        <button type='button' onClick={handleAdd}>Enviar</button>
+      </div>
+
+      {task.map(task => (
+        <div id='task_value'>
+          <span key={task}>{task} <button id='btn-del' type='button' onClick={handleDel}>Excluir</button></span>
+        </div>
+      ))}
+
+
     </div>
   );
 }
